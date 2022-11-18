285806cb1d398d3add22954ffc34e4295e07ab25
nibinpsreenivas <96453329+nibinpsreenivas@users.noreply.github.com>
2022-11-18 19:27:20 +0530

    Add files via upload

---
+++ DAY3/ans3.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class ans3
+{
+  
+  public static void main(String args[])
+  {
+    ans3 obj = new ans3();
+    Scanner ob = new Scanner(System.in);
+    System.out.println("enter the input to give the ASCII value of the given input as the output.");
+    char a = ob.next().charAt(0);
+    System.out.println((int)a);
+   }
+ }
+   
