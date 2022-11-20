5407bc319d617e7a6e17361701b4e3f0392e9703
nibinpsreenivas <96453329+nibinpsreenivas@users.noreply.github.com>
2022-11-20 12:05:14 +0530

    Add files via upload

---
+++ DAY5/ans5.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class ans5
+{
+  
+  public static void main(String args[])
+  {
+    ans5 obj = new ans5();
+    Scanner ob = new Scanner(System.in);
+    System.out.println("enter the input to check whether the given number is odd or even");
+    int a = ob.nextInt();
+    if(a%2==0)
+    {
+      System.out.println("Even");
+    
+    }
+    else
+    {
+      System.out.println("Odd");
+    
+    }
+   
+  }
+
+}
+   
