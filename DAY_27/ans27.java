478113c0ed8ed736c1a8b1e7db4d8d1129571d3e
NIBIN <96453329+nibinpsreenivas@users.noreply.github.com>
2022-12-12 18:08:33 +0530

    Add files via upload

---
+++ DAY_27/ans27.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class ans27
+{
+
+  public static void main(String args[])
+  {
+    ans27 obj = new ans27();
+    Scanner ob = new Scanner(System.in);
+         System.out.println("Input:");
+         int a = ob.nextInt();
+         int ans=a;
+         for(int i=1;i<=a;i++)
+         {
+            ans++;
+         }
+         System.out.println("output:"+ans);
+        
+    }
+  
+        
+}
+
+
+   
