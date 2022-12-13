e7575b9b7b8cfb8d67ad989f48db341893a48fcc
NIBIN <96453329+nibinpsreenivas@users.noreply.github.com>
2022-12-13 18:59:02 +0530

    Add files via upload

---
+++ DAY_28/ans28.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class ans28
+{
+
+  public static void main(String args[])
+  {
+     ans28 obj = new ans28();
+     Scanner ob = new Scanner(System.in);
+         System.out.println("Input:");
+         String s = ob.nextLine();
+         StringBuilder s1 = new StringBuilder();
+         s1.append(s);
+         s1.reverse();
+         System.out.println("output:"+s1);
+        
+    }
+    
+        
+}
+
+
+   
