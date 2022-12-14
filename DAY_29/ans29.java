01f37453ccbb66d4a27d12e81bfcc455889f1303
NIBIN <96453329+nibinpsreenivas@users.noreply.github.com>
2022-12-14 21:26:53 +0530

    Add files via upload

---
+++ DAY_29/ans29.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class ans29
+{
+
+  public static void main(String args[])
+  {
+     ans29 obj = new ans29();
+     Scanner ob = new Scanner(System.in);
+         System.out.println("Input string 1:");
+         String s1 = ob.nextLine();
+         System.out.println("Input string 2:");
+         String s2 = ob.nextLine();
+         
+         System.out.println("output:"+s1+s2);
+        
+    }
+    
+        
+}
+
+
+   
